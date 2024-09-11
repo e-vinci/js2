219aea7c0d62bc3e8bbf0d4faa0802a82f4040cf
e-baron <raphael.baroni@vinci.be>
2024-09-11 16:37:01 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2bea1b341fc506f47850.js"
+    "url": "webpack-runtime-c60a56233e2262d4498b.js"
   },
   {
     "url": "framework-2e5b211a15610374bd3d.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c09f860e235dff47d443da87647711cf"
+    "revision": "83c496b680e88ea3a9ce802dac53a200"
   },
   {
     "url": "manifest.webmanifest",
